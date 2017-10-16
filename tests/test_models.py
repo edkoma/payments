588682fd77125f9e1856207e37de284887df0bf2
sn938@nyu.edu <sn938@nyu.edu>
2017-10-15 22:07:13 -0400

    Added test cases for update payment code.

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,9 +1,11 @@
 import unittest
 import json
-from server import Payment, PaymentStatus, PaymentMethodType, PaymentMethod, app, db
+from flask_api import status
+from server import Payment, PaymentStatus, PaymentMethodType, PaymentMethod, app, db, DataValidationError
 import logging
 
-class TestPayments(unittest.TestCase):
+
+class TestModels(unittest.TestCase):
 
     def setUp(self):
         # Only log criticl errors
@@ -20,8 +22,8 @@ class TestPayments(unittest.TestCase):
         db.session.remove()
         db.drop_all()
 
-    def test_create_a_payment(self):
-        # Create a payment and assert that it exists
+    def test_construct_a_payment(self):
+        """Create a payment and assert that it exists and was properly initialized"""
         pm = PaymentMethod(method_type=PaymentMethodType.CREDIT)
         payment = Payment(user_id=0, order_id=0, status=PaymentStatus.UNPAID,
             method=pm)
@@ -31,27 +33,40 @@ class TestPayments(unittest.TestCase):
         self.assertEqual(payment.status, PaymentStatus.UNPAID)
         self.assertEqual(payment.method, pm)
 
-    def test_add_a_payment(self):
-        # Create a payment and add it to the database
+    def test_create_a_payment(self):
+        """Create a payment and add it to the database"""
         pm = PaymentMethod(method_type=PaymentMethodType.CREDIT)
         payment = Payment(user_id=0, order_id=0, status=PaymentStatus.UNPAID,
             method=pm)
         self.assertTrue(payment != None)
-        db.session.add(payment)
-        db.session.commit()
+        payment.save()
         self.assertEqual(payment.id, 1)
         # This should be the only payment in the database, sqlite starts primary keys at 1
-        p = Payment.query.get(1)
+        p = Payment.find(1)
         self.assertEqual(p, payment)
 
-     def test_get_payment(self):
-        resp = self.app.get('/payments/2')
-        #print 'resp_data: ' + resp.data
-        self.assertEqual( resp.status_code, status.HTTP_200_OK )
-        data = json.loads(resp.data)
-        self.assertEqual (data['user_id'], 'kitty')
-        self.assertEqual (data['order_id'], 'kitty')
-        self.assertEqual (data['status'], 'kitty')
-        self.assertEqual (data['method_id'], 'kitty')
-        self.assertEqual (data['method'], 'kitty')
+    def test_update_payment(self):
+        """Update an existing Payment"""
+        pm = PaymentMethod(method_type=PaymentMethodType.DEBIT)
+        new_payment = {'user_id': 0, "order_id": 0, 'status': PaymentStatus.UNPAID, 'method_id': method_id, 'method':}
+        new_payment = Payment(user_id=0, order_id=0, status=PaymentStatus.UNPAID, method=pm) 
+        data =json.dumps(new_payment)
+        resp = self.app.put('payments/1', data = data, content_type = 'application/json')
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        new_json = json.loads(resp.data)
+        self.assertEqual(new_json['method_type'], 2)
+
+    def test_update_payment_with_no_data(self):
+        """ Update a Payment with no data passed """
+        resp = self.app.put('/payments/2', data=None, content_type = 'application/json')
+        self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_update_payment_not_found(self):
+        """Update a Payment that does not exist"""
+        pm = PaymentMethod(method_type=PaymentMethodType.CREDIT)
+        new_payment = Payment(user_id=0, order_id=0, status=PaymentStatus.PAID, method=pm)
+        data = json.dumps(new_payment)
+        resp = self.app.put('/pets/3', data=data, content_type='application/json')
+        self.assertEquals(resp.status_code, status.HTTP_404_NOT_FOUND)
+
         
