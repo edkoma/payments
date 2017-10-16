48ded22f1d37c6cc8e90ebff914d591b1f5f36cd
sn938@nyu.edu <sn938@nyu.edu>
2017-10-15 22:05:57 -0400

    Added update payment code.

--- server.py
+++ server.py
@@ -1,12 +1,9 @@
-
-
 import os
 import logging
 from flask import Flask, Response, jsonify, request, json, make_response, url_for
 from flask_api import status
 from flask_sqlalchemy import SQLAlchemy
 from enum import Enum
-from werkzeug.exceptions import NotFound
 
 
 ######################################################################
@@ -30,6 +27,9 @@ class DataValidationError(ValueError):
     pass
 
 
+# This needs to be after db, classes, etc are initialized to avoid circular imports
+from models import *
+
 ######################################################################
 # Routes
 ######################################################################
@@ -50,79 +50,13 @@ def request_validation_error(e):
 def not_found(e):
     return make_response(jsonify(status=404, error='Not Found', message=e.description), status.HTTP_404_NOT_FOUND)
 
-
-######################################################################
-# Models
-######################################################################
-
-class PaymentStatus(Enum):
-    UNPAID = "unpaid"
-    PROCESSING = "processing"
-    PAID = "paid"
-    
-
-class PaymentMethodType(Enum):
-    CREDIT = "credit"
-    DEBIT = "debit"
-    PAYPAL = "paypal"
-
-def create():
-    pm = PaymentMethod(method_type=PaymentMethodType.CREDIT)
-    payment = Payment(user_id=0, order_id=0, status=PaymentStatus.UNPAID, method=pm)
-
-class Payment(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, nullable=False)
-    order_id = db.Column(db.Integer, nullable=False)
-    status = db.Column(db.Enum(PaymentStatus))
-    method_id = db.Column(db.Integer, db.ForeignKey('payment_method.id'), nullable=False)
-    method = db.relationship('PaymentMethod', backref=db.backref('payments', lazy=True))
-    
-    def __repr__(self):
-        return '<Payment %d>' % self.id
-
-
-    def self_url(self):
-        return url_for('get_payments', id=self.id, _external=True)
-
-    def serialize(self):
-        return {"id": self.id, "user_id": self.user_id, "order_id": self.order_id,
-                "status": self.status, "method_id": self.method_id, "method": self.method}
-
-    def serialize_methods(self):
-        return{"method_id": self.method_id, "method": self.method}
-    
-    def deserialize(self, data):
-        try:
-            self.id = data['id']
-            self.user_id = data['user_id']
-            self.order_id = data['order_id']
-            self.status = data['status']
-            self.method_id = data['method_id']
-            self.method = data['method']
-        except KeyError as e:
-            raise DataValidationError('Invalid payment: missing ' + e.args[0])
-        except TypeError as e:
-            raise DataValidationError('Invalid payment: body of request contained bad or no data')
-        return self
-
-
-class PaymentMethod(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    method_type = db.Column(db.Enum(PaymentMethodType))
-
-    def __repr__(self):
-        return '<PaymentMethod %d, type %r>' % (self.id, self.method_type)
-
 ######################################################################
 # LIST ALL PAYMENTS
 ######################################################################
 @app.route('/payments', methods=['GET'])
 def list_payments():
-    payments = []
-
-    results = [payments.serialize() for payments in payments]
-    print results
+    payments = Payment.all()
+    results = [payment.serialize() for payment in payments]
     return make_response(jsonify(results), status.HTTP_200_OK)
 
 ######################################################################
@@ -130,22 +64,26 @@ def list_payments():
 ######################################################################
 @app.route('/payments/methods', methods=['GET'])
 def list_payment_methods():
-    payment_methods = []
-    method = Payment.query.get('method')
-    if method:
-        payment_methods = Payment.query.filter(Payment.method == method)
-
-    results = [p.serialize_methods() for p in payment_methods]
-    return make_response(jsonify(results), status.HTTP_200_OK)
+    payment_methods = [pm.value() for pm in PaymentMethodType]
+    return make_response(jsonify(payment_methods), status.HTTP_200_OK)
 
 ######################################################################
 # RETRIEVE A PAYMENT
 ######################################################################
 @app.route('/payments/<int:id>', methods=['GET'])
 def get_payments(id):
-    payment = Payment.query.get(id)
-    if not payment:
-        raise NotFound("Payment with id: '{}' was not found in the database.".format(id))
+    payment = Payment.find_or_404(id)
+    return make_response(jsonify(payment.serialize()), status.HTTP_200_OK)
+
+######################################################################
+# UPDATE AN EXISTING PAYMENT
+######################################################################
+@app.route('/payments/<int:id>', methods=['PUT'])
+def update_payments(id):
+    payment = Payment.find_or_404(id)
+    payment.deserialize(requst.get_json())
+    payment.id = id
+    payment.save()
     return make_response(jsonify(payment.serialize()), status.HTTP_200_OK)
 
 ######################################################################
@@ -155,8 +93,7 @@ def get_payments(id):
 def create_payment():
     payment = Payment()
     payment.deserialize(request.get_json())
-    db.session.add(payment)
-    db.session.commit()
+    payment.save()
     message = payment.serialize()
     return make_response(jsonify(message), status.HTTP_201_CREATED, {'Location': payment.self_url() })
 
@@ -166,5 +103,4 @@ def create_payment():
 
 if __name__ == "__main__":
     db.create_all()
-    create()
     app.run(host='0.0.0.0', port=int(PORT), debug=DEBUG)
