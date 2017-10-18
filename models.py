1a271046627845a0bbd735240823509a53bfafe9
Jordan Green <thegreenjordan@gmail.com>
2017-10-18 03:50:19 +0000

    Add CRUD calls for PaymentMethod and set-default API call

--- models.py
+++ models.py
@@ -64,7 +64,7 @@ class Payment(db.Model):
         return Payment.query.filter(Payment.order_id == order_id)
 
     def self_url(self):
-        return url_for('get_payments', id=self.id, _external=True)
+        return url_for('get_payment', id=self.id, _external=True)
 
     def serialize(self):
         return {"id": self.id, "user_id": self.user_id, "order_id": self.order_id,
@@ -89,6 +89,62 @@ class Payment(db.Model):
 class PaymentMethod(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     method_type = db.Column(db.Enum(PaymentMethodType))
+    is_default = db.Column(db.Boolean, default=False)
+
+    def save(self):
+        """ Saves an existing Payment Method in the database """
+        # if the id is None it hasn't been added to the database
+        if not self.id:
+            db.session.add(self)
+        db.session.commit()
+
+    @staticmethod
+    def find(id):
+        """ Find a Payment Method by its id """
+        return PaymentMethod.query.get(id)
+
+    def delete(self):
+        """ Delete a Payment Methodfrom the database"""
+        db.session.delete(self)
+        db.session.commit()
+
+    @staticmethod
+    def all():
+        """ Return all of the Payment Methods in the database """
+        return PaymentMethod.query.all()
+
+    @staticmethod
+    def find_or_404(payment_id):
+        """ Find a Payment by its id """
+        return PaymentMethod.query.get_or_404(payment_id)
+
+    def self_url(self):
+        return url_for('get_payment_method', id=self.id, _external=True)
+
+    def serialize(self):
+        return {"id": self.id, "method_type": self.method_type.value, "is_default": self.is_default}
+
+    def deserialize(self, data):
+        try:
+            # Won't have an id yet if this is when it's being created for the first time
+            if 'id' in data:
+                self.id = data['id']
+            self.method_type = PaymentMethodType(data['method_type'])
+            if 'is_default' in data:
+                self.is_default = data['is_default']
+            else:
+                self.is_default = False
+        except KeyError as e:
+            raise DataValidationError('Invalid payment method: missing ' + e.args[0])
+        except (TypeError, ValueError) as e:
+            raise DataValidationError('Invalid payment method: body of request contained bad or no data')
+        return self
+
+    def set_default(self):
+        """Sets a payment method to be the default"""
+        self.is_default = True
+        self.save()
+
 
     def __repr__(self):
         return '<PaymentMethod %d, type %r>' % (self.id, self.method_type)
--- tests/test_server.py
+++ tests/test_server.py
@@ -22,9 +22,9 @@ class TestServer(unittest.TestCase):
         db.drop_all()
 
     def test_get_home(self):
-    	"""GET the home page"""
-    	resp = self.app.get('/')
-    	self.assertTrue('Payments Home Page' in resp.data)
+        """GET the home page"""
+        resp = self.app.get('/')
+        self.assertTrue('Payments Home Page' in resp.data)
 
     def test_post_a_payment(self):
         """Create a payment using a POST"""
@@ -75,7 +75,7 @@ class TestServer(unittest.TestCase):
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_get_all_payments(self):
-    	"""GET all payments in the database"""
+        """GET all payments in the database"""
         js = {'user_id': 0, 'order_id': 0, 'status': PaymentStatus.UNPAID.value,
             'method_id': PaymentMethodType.CREDIT.value}
         resp = self.app.post('/payments', data=json.dumps(js), content_type='application/json')
@@ -88,15 +88,6 @@ class TestServer(unittest.TestCase):
         resp = self.app.get('/payments')
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         self.assertEqual(len(json.loads(resp.data)), 2)
-
-    def test_list_all_payment_methods(self):
-        """Get all payment methods"""
-        resp = self.app.get('/payments/methods')
-        self.assertEqual(resp.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(json.loads(resp.data)), len(PaymentMethodType))
-        paymentMethodList = set(pm.name for pm in PaymentMethodType)
-        responseList = set(json.loads(resp.data))
-        self.assertTrue(paymentMethodList == responseList)
 
     def test_update_payment(self):
         """Update an existing Payment"""
@@ -187,3 +178,124 @@ class TestServer(unittest.TestCase):
         resp = self.app.delete('/payments/0', content_type='application/json')
         self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(len(resp.data), 0)
+
+    def test_get_a_payment_method(self):
+        """Create a payment method, then GET it"""
+        js = {'method_type': PaymentMethodType.CREDIT.value}
+        resp = self.app.post('/payments/methods', data=json.dumps(js), content_type='application/json')
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+        resp = self.app.get('/payments/methods/1')
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        pm = PaymentMethod()
+        pm.deserialize(json.loads(resp.data))
+        self.assertEqual(pm.id, 1)
+        self.assertEqual(pm.method_type, PaymentMethodType(js['method_type']))
+        self.assertEqual(pm.is_default, False)
+
+    def test_get_a_payment_method_404(self):
+        """Try to GET a payment method that doesn't exist"""
+        resp = self.app.get('/payments/methods/1')
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_get_all_payment_methods(self):
+        """Get all payment methods"""
+        js = {'method_type': PaymentMethodType.CREDIT.value}
+        resp = self.app.post('/payments/methods', data=json.dumps(js), content_type='application/json')
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+        js = {'method_type': PaymentMethodType.DEBIT.value}
+        resp = self.app.post('/payments/methods', data=json.dumps(js), content_type='application/json')
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+        resp = self.app.get('/payments/methods')
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(json.loads(resp.data)), 2)
+
+    def test_post_a_payment_method(self):
+        """Create a payment method using a POST"""
+        js = {'method_type': PaymentMethodType.CREDIT.value}
+        resp = self.app.post('/payments/methods', data=json.dumps(js), content_type='application/json')
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+        get_resp = self.app.get('/payments/methods/1')
+        pm = PaymentMethod()
+        pm.deserialize(json.loads(get_resp.data))
+        self.assertEqual(pm.id, 1)
+
+    def test_post_an_invalid_payment_method(self):
+        """POST a payment method with invalid information"""
+        js = {'method_type': 'not_a_method_type'}
+        resp = self.app.post('/payments/methods', data=json.dumps(js), content_type='application/json')
+        self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertTrue('body of request contained bad or no data' in resp.data)
+
+    def test_post_a_payment_method_with_missing_information(self):
+        """POST a payment method with missing information"""
+        resp = self.app.post('/payments/methods', data=None, content_type='application/json')
+        self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_delete_payment_method(self):
+        """ Delete a payment method """
+        # First insert a payment method
+        js = {'method_type': PaymentMethodType.CREDIT.value}
+        resp = self.app.post('/payments/methods', data=json.dumps(js), content_type='application/json')
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+        # Now delete the payment method
+        resp = self.app.delete('/payments/methods/1', content_type='application/json')
+        self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(len(resp.data), 0)
+        # Assert that no payment methods are present
+        resp = self.app.get('/payments/methods')
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(json.loads(resp.data)),0)
+
+    def test_delete_payment_method_not_found(self):
+        """ Delete a payment method that does not exist"""
+        #Assert that no payment methodss exist
+        resp = self.app.get('/payments/methods')
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(json.loads(resp.data)),0)
+        #Call delete for any id
+        resp = self.app.delete('/payments/methods/0', content_type='application/json')
+        self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(len(resp.data), 0)
+
+    def test_update_payment_method(self):
+        """Update an existing Payment Method"""
+        js = {'method_type': PaymentMethodType.CREDIT.value}
+        resp = self.app.post('/payments/methods', data=json.dumps(js), content_type='application/json')
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+        js = {'method_type': PaymentMethodType.DEBIT.value}
+        resp = self.app.put('payments/methods/1', data=json.dumps(js), content_type='application/json')
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        new_json = json.loads(resp.data)
+        self.assertEqual(new_json['method_type'], PaymentMethodType.DEBIT.value)
+
+    def test_update_payment_method_with_no_data(self):
+        """ Update a Payment Method with no data passed """
+        js = {'method_type': PaymentMethodType.CREDIT.value}
+        resp = self.app.post('/payments/methods', data=json.dumps(js), content_type='application/json')
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+        resp = self.app.put('/payments/methods/1', data=None, content_type='application/json')
+        self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_update_payment_method_not_found(self):
+        """Update a Payment Method that does not exist"""
+        js = {'method_type': PaymentMethodType.CREDIT.value}
+        resp = self.app.put('/payments/methods/4', data=json.dumps(js), content_type='application/json')
+        self.assertEquals(resp.status_code, status.HTTP_404_NOT_FOUND)
+
+
+    def test_set_default_payment_method(self):
+        """Set default payment method through an API call"""
+        # Create a new payment mehtod and assert it is not the default
+        js = {'method_type': PaymentMethodType.CREDIT.value}
+        resp = self.app.post('/payments/methods', data=json.dumps(js), content_type='application/json')
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+        resp = self.app.get('/payments/methods/1')
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        pm = PaymentMethod()
+        pm.deserialize(json.loads(resp.data))
+        self.assertFalse(pm.is_default)
+        # Now set it to True
+        resp = self.app.put('/payments/methods/1/set-default')
+        pm = PaymentMethod()
+        pm.deserialize(json.loads(resp.data)[0])
+        self.assertTrue(pm.is_default)
