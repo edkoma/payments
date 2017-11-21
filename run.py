8b20a8ae6d14b8c5279cbaf4fb93577f2823a7be
edkoma <edward.zc.wang@gmail.com>
2017-11-21 10:54:51 -0500

    added run.py

---
+++ run.py
@@ -0,0 +1,22 @@
+"""
+Payment Service Runner
+Start the Payment Service and initializes logging
+"""
+
+import os
+from app import app, server
+
+# Pull options from environment
+DEBUG = (os.getenv('DEBUG', 'False') == 'True')
+PORT = os.getenv('PORT', '5000')
+
+######################################################################
+#   M A I N
+######################################################################
+if __name__ == "__main__":
+    print "************************************************************"
+    print "        P A Y M E N T   R E S T   A P I   S E R V I C E "
+    print "************************************************************"
+    server.initialize_logging()
+    server.init_db()
+    app.run(host='0.0.0.0', port=int(PORT), debug=DEBUG)
