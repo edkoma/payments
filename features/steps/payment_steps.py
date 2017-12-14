d24f5e8537f24014a9c46a578f9d8949742fcced
edkoma <edward.zc.wang@gmail.com>
2017-12-13 21:40:33 -0500

    list all payments passed

--- features/steps/payment_steps.py
+++ features/steps/payment_steps.py
@@ -54,7 +54,7 @@ def step_impl(context, button):
     button_id = button.lower() + '-btn'
     context.driver.find_element_by_id(button_id).click()
 
-@then(u'I should see "{name}" in the results')
+@then(u'I should see user_id "{name}" in the results')
 def step_impl(context, name):
     #element = context.driver.find_element_by_id('search_results')
     #expect(element.text).to_contain(name)
